62f90b24a9a8812bcb32ed9db05c4aba15080735
AxelBerrelleza <xrodriguezberrelleza@gmail.com>
2025-02-27 21:06:25 -0600

    using rich.Table in stock seach command

--- commands/stocks/__init__.py
+++ commands/stocks/__init__.py
@@ -1,9 +1,23 @@
 import typer
 from .morning_star import *
-from pprint import pprint
+from rich.console import Console
+from rich.table import Table
 
 stocks_app = typer.Typer()
+console = Console()
 
 @stocks_app.command()
 def search(text: str):
-    pprint(morning_star.autocomplete(text))
+    response = morning_star.autocomplete(text)
+
+    table = Table("Name", "Region & symbol", "Type", "Exchange", "Performance Id")
+    for row in response:
+        table.add_row(
+            row["Name"],
+            row["RegionAndTicker"],
+            row["TypeName"],
+            row["ExchangeShortName"],
+            row["PerformanceId"]
+        )
+
+    console.print(table)
