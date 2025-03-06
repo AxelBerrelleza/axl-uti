fae73beb8bd1000ef65963913544b8d31fec6d0e
AxelBerrelleza <xrodriguezberrelleza@gmail.com>
2025-03-05 20:24:48 -0600

    created pytest.ini + start checking excel file creted by stocks comparator command

--- commands/tests/stocks/test_comparator.py
+++ commands/tests/stocks/test_comparator.py
@@ -1,20 +1,33 @@
 import pytest
 from typer.testing import CliRunner
-from commands.stocks import stocks_app, Endpoints, getOverview
+from logging import getLogger
+from commands.stocks import stocks_app, Endpoints
 from commands.tests.fixtures import APIResponses
+from commands.stocks.SpreedSheetComparation import SpreadSheetComparation
+from openpyxl import load_workbook
+from openpyxl.worksheet.worksheet import Worksheet
 
 runner = CliRunner()
+logger = getLogger(__name__)
 
 def test_help():
     result = runner.invoke(stocks_app, ['comparator', '--help'])
     assert result.exit_code == 0
     assert result.stdout != None
 
-# def test_creation_of_xl_file():
-def test_mock(requests_mock):
+def test_command(requests_mock):
     requests_mock.get(Endpoints.OVERVIEW, json=APIResponses.MS.OVERVIEW)
-    result = runner.invoke(stocks_app, ['comparator', 'GOOGL', 'AMZN'])
+    symbols = ['GOOGL', 'AMZN']
+    result = runner.invoke(stocks_app, ['comparator', *symbols])
     
     print(result)
     assert 'Loading' in result.stdout
     assert 'Finished' in result.stdout
+    
+    xlDoc = load_workbook(filename='comparation.xlsx', data_only=True)
+    sheet: Worksheet = xlDoc.active
+    for key, symbol in enumerate(symbols):
+        assert sheet.cell(row=2, column=SpreadSheetComparation.initialColumn + key).value == symbol
+        
+    for cells in sheet.iter_cols(min_col=SpreadSheetComparation.initialColumn, min_row=3, max_col=4, max_row=15):
+        logger.info(list(( c.value for c in cells )))
